d7d609d2538fff01188e2f1ad5eb906408ed83b1
Krishnapushpan <krishnapushpan2003@gmail.com>
2025-01-22 15:38:26 +0530

    commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import {createBrowserRouter,createRoutesFromElements,RouterProvider,Route,} from 'react-router-dom';
 import Home from './pages/Home';
-
+import Addtask from './pages/AddTask';
 const App = () => {
-const router = createBrowserRouter(createRoutesFromElements(
+const router = createBrowserRouter(createRoutesFromElements(<>
   <Route path="/" element={<Home/>}/>
+  <Route path="/add-task" element={<Addtask/>}/>
+  </>
 ))
   return (
     <RouterProvider router={router}></RouterProvider>
